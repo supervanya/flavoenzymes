f88132c1689a0f5ed60772fef718c27e9fc8933e
supervanya <vano@umich.edu>
2020-04-26 12:37:28 -0400

    cleanup

--- GLOBALS.py
+++ GLOBALS.py
@@ -1,5 +1,5 @@
 DEBUG = True
 USE_CACHING = True
 
-# less_verbose <---- 'none' | 'error' | 'warning' | 'info' | 'debug' ----> more_verbose
-VERBOSITY = 'error' # set to 'none' to have almost no output
+# less_verbose <---- 'none' | 'success' | 'error' | 'warning' | 'info' | 'debug' | 'silly' ----> more_verbose
+VERBOSITY = 'info' # set to 'none' to have almost no output, set to 'silly' for most verbosity
--- helpers.py
+++ helpers.py
@@ -16,22 +16,13 @@ verbosity_to_levels = {
 
 verbosity_to_color = {
     'none':    'none',
-    'success': f'{Fore.GREEN}✔ SUCCESS{Fore.RESET}',
-    'error':   f'{Fore.RED}⨯ ERROR{Fore.RESET}',
-    'warning': f'{Fore.YELLOW}⚠ WARNING{Fore.RESET}',
-    'info':    f'{Fore.GREEN}ℹ INFO{Fore.RESET}',
-    'debug':   f'{Fore.BLUE}• DEBUG{Fore.RESET}',
-    'silly':   f'SILLY'
+    'success': f'{Fore.GREEN}✔SUCCESS{Fore.RESET}',
+    'error':   f'{Fore.RED}⨯ERROR{Fore.RESET}',
+    'warning': f'{Fore.YELLOW}⚠WARNING{Fore.RESET}',
+    'info':    f'{Fore.GREEN}ℹINFO{Fore.RESET}',
+    'debug':   f'{Fore.BLUE}•DEBUG{Fore.RESET}',
+    'silly':   f'{Fore.MAGENTA}~SILLY{Fore.RESET}',
     }
-
-def print_percent_done(index, length):
-    percent_done = round((index+1)/length*100)
-    done = (percent_done/10)
-    done_str = '█'*int(done)
-    togo = (10-(percent_done/10))
-    togo_str = '░'*int(togo)
-    print(f'\n\n----> Progress: {done_str}{togo_str} \t{percent_done}% done')
-
 
 def log(message, verbosity):
     if should_log(verbosity):
@@ -48,3 +39,12 @@ def should_log(message_verbosity):
     if message_level <= global_level:
         return True
     return False
+
+
+def print_percent_done(index, length):
+    percent_done = round((index+1)/length*100)
+    done = (percent_done/10)
+    done_str = '█'*int(done)
+    togo = (10-(percent_done/10))
+    togo_str = '░'*int(togo)
+    print(f'\n\n----> Progress: {done_str}{togo_str} \t{percent_done}% done')
