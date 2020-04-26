cd6515704c7fe829d34290c39a062bd7ba3124f5
supervanya <vano@umich.edu>
2020-04-26 12:01:25 -0400

    added colors and icons to logger + new level `success`

--- helpers.py
+++ helpers.py
@@ -1,15 +1,28 @@
 from GLOBALS import VERBOSITY
 from datetime import datetime
+from colorama import Fore, Back, Style, init, deinit
 
 
 # ========== LOGGING ===========
 verbosity_to_levels = {
     'none': 0,
-    'error': 1,
-    'warning': 2,
-    'info': 3,
-    'debug': 4,
+    'success': 1,
+    'error': 2,
+    'warning': 3,
+    'info': 4,
+    'debug': 5,
+    'silly': 6,
 }
+
+verbosity_to_color = {
+    'none':    'none',
+    'success': f'{Fore.GREEN}✔ SUCCESS{Fore.RESET}',
+    'error':   f'{Fore.RED}⨯ ERROR{Fore.RESET}',
+    'warning': f'{Fore.YELLOW}⚠ WARNING{Fore.RESET}',
+    'info':    f'{Fore.GREEN}ℹ INFO{Fore.RESET}',
+    'debug':   f'{Fore.BLUE}• DEBUG{Fore.RESET}',
+    'silly':   f'SILLY'
+    }
 
 def print_percent_done(index, length):
     percent_done = round((index+1)/length*100)
@@ -24,7 +37,8 @@ def log(message, verbosity):
     if should_log(verbosity):
         now = datetime.now()
         current_time = now.strftime("%H:%M:%S")
-        print(f'{current_time} [{verbosity}]: {message}')
+        verb_color = verbosity_to_color[verbosity]
+        print(f'{current_time} [{verb_color}]:\t{message}')
         
 def should_log(message_verbosity):
     # print(f'verbosity: {message_verbosity}, VERBOSITY: {VERBOSITY}')
