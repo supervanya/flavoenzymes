769042dfa786d0261964515b77c221bc0f66dd5d
supervanya <vano@umich.edu>
2020-04-26 21:55:13 -0400

    refctor

--- modules/helpers/logger.py
+++ modules/helpers/logger.py
@@ -1,7 +1,7 @@
-from GLOBALS import VERBOSITY
 from datetime import datetime
 from colorama import Fore, Back, Style, init, deinit
 
+from GLOBALS import VERBOSITY
 
 # ========== LOGGING ===========
 verbosity_to_levels = {
@@ -39,12 +39,3 @@ def should_log(message_verbosity):
     if message_level <= global_level:
         return True
     return False
-
-
-def print_percent_done(index, length):
-    percent_done = round((index+1)/length*100)
-    done = (percent_done/10)
-    done_str = '█'*int(done)
-    togo = (10-(percent_done/10))
-    togo_str = '░'*int(togo)
-    print(f'\n\n----> Progress: {done_str}{togo_str} \t{percent_done}% done')
