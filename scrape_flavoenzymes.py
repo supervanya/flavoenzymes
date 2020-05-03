9f8f7ce264092270bc22d7ea295b6d1a3d848754
supervanya <vano@umich.edu>
2020-05-03 11:30:54 -0400

    linter + comments

--- scrape_flavoenzymes.py
+++ scrape_flavoenzymes.py
@@ -6,24 +6,26 @@
 # print(dir(scrape_brenda))
 # scrape_brenda.printThis('khskd')
 # import modules.scrapers
-# print(scrape_brenda) 
+# print(scrape_brenda)
 
-from modules.scrapers.scrape_driver import create_list
+from modules.scrapers.scrape_driver import create_missing_list
 from modules.scrapers.scrape_driver import scrape_all
 from GLOBALS import KEYWORDS
 
-def main():
-    # TODO: still need to fetch past data
-    # takes into account the whitelist and blacklist
-    missing_list = create_list(prev_list=set(), kegg_keywords=KEYWORDS, brenda_keywords=KEYWORDS)
 
-    # searches brenda
-    # searches kegg
+def main():
+    # TODO: still need to read past data to see prev_list
+    # takes into account the whitelist and blacklist
+    missing_list = create_missing_list(
+        kegg_keywords=KEYWORDS, brenda_keywords=KEYWORDS)
+
+    # searches brenda and kegg
     scraped_results = scrape_all(missing_list)
 
     # write it out to a file
-    
+
     pass
 
+
 if __name__ == "__main__":
-    main();        
+    main()
